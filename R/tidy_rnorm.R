73a7a4f5a78bce6fe912901f5ad914de870b51de
Steven Paul Sanderson II, MPH <spsanderson@users.noreply.github.com>
2022-01-16 09:24:47 -0500

    Create tidy_rnorm.R

---
+++ R/tidy_rnorm.R
@@ -0,0 +1,20 @@
+#' Tidy Gaussian
+#'
+#' @family Data Generator
+#'
+#' @author Steven P. Sanderson II, MPH
+#'
+#' @details
+#'
+#' @description
+#'
+#' @param
+#'
+#' @examples
+#'
+#' @return
+#'
+#' @export
+#'
+
+tidy_rnorm <- function(.n = 50, .mean = 0, .sd = 0, .num_walks = 1)
