46a6549e3cc19913e175f4f9cbcd26339beebbfe
Steven Paul Sanderson II, MPH <spsanderson@users.noreply.github.com>
2022-01-18 09:11:20 -0500

    Update random-tidy-rnorm.R
    fix typos

--- R/random-tidy-rnorm.R
+++ R/random-tidy-rnorm.R
@@ -9,7 +9,7 @@
 #' @details This function uses the underlying `stats::rnorm()`, `stats::pnorm()`,
 #' and `stats::qnorm()` functions to generate data from the given parameters.
 #'
-#' @description This function will generate `n` randong points from a gaussian
+#' @description This function will generate `n` random points from a Gaussian
 #' distribution with a user provided, mean, standard deviation and number of
 #' random simulations to be produced. The function returns a tibble with the
 #' simulation number column the x column which corresponds to the n randomly
