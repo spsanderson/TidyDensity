bedef884ef436f5dd679fbf8fd092d96115c35ea
Steven Paul Sanderson II, MPH <spsanderson@users.noreply.github.com>
2022-01-18 09:15:06 -0500

    Update random-tidy-gamma.R

--- R/random-tidy-gamma.R
+++ R/random-tidy-gamma.R
@@ -1,4 +1,37 @@
-
+#' Tidy Randomly Generated Gaussian Tibble
+#'
+#' @family Data Generator
+#'
+#' @author Steven P. Sanderson II, MPH
+#'
+#' @seealso \url{hhttps://www.statology.org/fit-gamma-distribution-to-dataset-in-r/}
+#' @seealso \url{https://en.wikipedia.org/wiki/Gamma_distribution}
+#'
+#' @details This function uses the underlying `stats::rgamma()`, and its underlying
+#' `p`, `d`, and `q` functions.
+#'
+#' @description This function will generate `n` random points from a gamma
+#' distribution with a user provided, shape, rate, and number of
+#' random simulations to be produced. The function returns a tibble with the
+#' simulation number column the x column which corresponds to the n randomly
+#' generated points, the `d_`, `p_` and `q_` data points as well.
+#'
+#' The data is returned un-grouped.
+#'
+#' @param .n The number of randomly generated points you want.
+#' @param .shape The mean of the randomly generated data. This is strictly 0 to infinity.
+#' @param .rate The standard deviation of the randomly generated data. This is
+#' strictly from 0 to infinity.
+#' @param .num_sims The number of randomly generated simulations you want.
+#'
+#' @examples
+#' tidy_normal()
+#'
+#' @return
+#' A tibble of randomly generated.
+#'
+#' @export
+#'
 
 tidy_gamma <- function(.n = 50, .shape = 1, .rate = 1, .num_sims = 1){
 
@@ -25,7 +58,7 @@ tidy_gamma <- function(.n = 50, .shape = 1, .rate = 1, .num_sims = 1){
 
     if(!is.numeric(shp) | shp < 0){
         rlang::abort(
-            "The parameters of '.shapte' and '.rate' must be of class numeric.
+            "The parameters of '.shape' and '.rate' must be of class numeric.
             Please pass a numer like 1 or 1.1 etc. and must be greater than 0."
         )
     }
