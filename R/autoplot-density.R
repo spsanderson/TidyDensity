f94247081e30ee8bf1a30ab93f277c8ee21a5544
Steven Paul Sanderson II, MPH <spsanderson@users.noreply.github.com>
2022-01-20 09:36:41 -0500

    update site

--- R/autoplot-density.R
+++ R/autoplot-density.R
@@ -68,7 +68,8 @@ tidy_autoplot <- function(.data, .plot_type = "density", .line_size = .5, .point
     if(!attributes(.data)$tibble_type %in% c(
         "tidy_gaussian", "tidy_poisson","tidy_gamma","tidy_beta","tidy_f",
         "tidy_hypergeometric","tidy_lognormal","tidy_cauchy","tidy_chisquare",
-        "tidy_weibull","tidy_uniform","tidy_logistic","tidy_exponential"
+        "tidy_weibull","tidy_uniform","tidy_logistic","tidy_exponential",
+        "tidy_empirical"
     )){
         rlang::abort("The data passed must come from a `tidy_` distribution function.")
     }
@@ -119,6 +120,8 @@ tidy_autoplot <- function(.data, .plot_type = "density", .line_size = .5, .point
             paste0("Location: ", atb$.location, " - Scale: ", atb$.scale)
         } else if(atb$tibble_type == "tidy_exponential"){
             paste0("Rate: ", atb$.rate)
+        } else if(atb$tibble_type == "tidy_empirical"){
+            paste0("Empirical - No params")
         }
     )
 
