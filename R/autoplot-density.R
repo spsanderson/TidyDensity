fb6f349155d7ae003067f9ae486b4afafb002ee3
Steven Paul Sanderson II, MPH <spsanderson@users.noreply.github.com>
2022-01-25 13:27:10 -0500

    Fix #33

--- R/autoplot-density.R
+++ R/autoplot-density.R
@@ -71,7 +71,7 @@ tidy_autoplot <- function(.data, .plot_type = "density", .line_size = .5, .point
         "tidy_gaussian", "tidy_poisson","tidy_gamma","tidy_beta","tidy_f",
         "tidy_hypergeometric","tidy_lognormal","tidy_cauchy","tidy_chisquare",
         "tidy_weibull","tidy_uniform","tidy_logistic","tidy_exponential",
-        "tidy_empirical","tidy_binomial","tidy_geometric"
+        "tidy_empirical","tidy_binomial","tidy_geometric","tidy_negative_binomial"
     )){
         rlang::abort("The data passed must come from a `tidy_` distribution function.")
     }
@@ -90,6 +90,7 @@ tidy_autoplot <- function(.data, .plot_type = "density", .line_size = .5, .point
     n <- atb$.n
     sims <- atb$.num_sims
     dist_type <- stringr::str_remove(atb$tibble_type, "tidy_") %>%
+        stringr::str_replace_all(pattern = "_", " ") %>%
         stringr::str_to_title()
 
     sub_title <- paste0(
@@ -124,7 +125,7 @@ tidy_autoplot <- function(.data, .plot_type = "density", .line_size = .5, .point
             paste0("Rate: ", atb$.rate)
         } else if(atb$tibble_type == "tidy_empirical"){
             paste0("Empirical - No params")
-        } else if(atb$tibble_type == "tidy_binomial"){
+        } else if(atb$tibble_type %in% c("tidy_binomial","tidy_negative_binomial")){
             paste0("Size: ", atb$.size, " - Prob: ", atb$.prob)
         } else if(atb$tibble_type == "tidy_geometric"){
             paste0("Prob: ", atb$.prob)
