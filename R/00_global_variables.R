3dfc09d4e1978e094849701dd3642f2f39c3a036
Steven Paul Sanderson II, MPH <spsanderson@users.noreply.github.com>
2022-03-08 11:53:39 -0500

    Update 00_global_variables.R

--- R/00_global_variables.R
+++ R/00_global_variables.R
@@ -1,6 +1,7 @@
 globalVariables(
   names = c(
     "d", "density", "dx", "dy", "p", "sd", "sim_number", "x", "y",
-    ".n",".num_sims","dist_name","random_walk_value","results"
+    ".n",".num_sims","dist_name","random_walk_value","results",
+    "dist_type","var","shape"
   )
 )
